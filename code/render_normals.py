0e174829a1766632323830ef07e342711c9ba7eb
hahayun3325 <hahayunc3c3y25@gmail.com>
2026-02-16 21:48:55 -0600

    New Object Geometry Module - Continue: after abalation study(7efd36fe5)

--- code/render_normals.py
+++ code/render_normals.py
@@ -263,7 +263,7 @@ python render_normals.py \
   --agent_id -1
 python render_normals.py \
   --case hold_MC1_ho3d \
-  --load_ckpt logs/1dc871e9f_000030000/checkpoints/last.ckpt \
+  --load_ckpt logs/7efd36fe5_000030000/checkpoints/last.ckpt \
   --config confs/render_stage3_hold_MC1_ho3d_sds_from_official.yaml \
   --mute \
   --agent_id -1
